8a6bf07fdb8981a2ee03edb7c15255f5fa83d1d1
chatterjeesurupa <chatterjee.su@husky.neu.edu>
2018-08-08 03:31:27 -0400

    changed access control

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "http://localhost:4200");
+        /*"http://localhost:4200");*/
+        "https://course-mgmt-angular-client-stc.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
